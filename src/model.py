b93424f371492c5a1e927219616b651b7ddd6ca4
joseph rance <t-jrance@microsoft.com>
2023-09-14 11:40:46 +0100

    add training skeleton

---
+++ src/model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+class MNISTCNN(nn.Module):
+
+    def __init__(self, classes=10):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(5, 10),
+            nn.ReLU(inplace=True),
+            nn.Linear(10, 1),
+        )
+
+    def forward(self, x):
+        return self.net(x)
--- src/simulate.py
+++ src/simulate.py
@@ -1,8 +1,32 @@
-def train():
+def train(environment, model):
     # 0. simulate a full run using our model and the environment
     #     - for each timestep (hour) we want to store the current state and the reward
     # 1. compute the "value" of each state based on the rewards we stored
     # 2. train the model to map from state -> reward
+
+    # this is the optimiser we will use to train
+    optimiser = SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    model.train()  # put the model in "training mode"
+
+    for episode in range(num_episodes):  # each iteration is one day simulation
+
+        dataset = # simulate to get dataset of tuple for each timestep (state + #VMs, value)
+
+        # this allows us to efficiently load the data into our model
+        loader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+        epoch_loss = total = correct = 0
+        for x, y in loader:  # train on each pair of input, value in the loader
+            x, y = x.to("cuda"), y.to("cuda")  # (for )
+
+            optimiser.zero_grad()
+
+            z = self.model(x)  # get model's prediction
+            loss = F.cross_entropy(z, y)  # get the loss between the model's prediction and the true value
+
+            loss.backward()
+            optimiser.step()  # update the model based on the loss
 
 def predict():
     # inputs:
