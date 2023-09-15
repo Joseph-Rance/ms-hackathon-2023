a50376dc804c7bd424587453273d24704513f033
Adnan Rashid <t-adrashid@microsoft.com>
2023-09-15 10:52:06 +0100

    Added saving of graph to folder, updated git ignore to not include graphs folder in commits

--- src/visualisation.py
+++ src/visualisation.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.pyplot as plt
 
 def visualise_data_processed(data_processed):
@@ -9,5 +10,9 @@ def visualise_data_processed(data_processed):
     plt.ylabel('Data processed')
     plt.title('Data processed over a day')
 
-    # Show the plot
-    plt.show()
+    # If graphs folder doesn't exist make it
+    if not os.path.exists('src/graphs'):
+        os.makedirs('src/graphs')
+    
+    # Save graph to folder
+    plt.savefig('src/graphs/data_processed_day.png')
